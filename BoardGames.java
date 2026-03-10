5b00d7271d39a8097cae7baf9c3b72b0df9c5482
Aeshah ♥ <aeshahsaeed@icloud.com>
2026-03-10 22:03:45 +0300

    Update BoardGames.java

--- BoardGames.java
+++ BoardGames.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.as;
 import java.util.Scanner;
 /**
  *
- * @author ashmohammed
+ * @author Awashah
  */
 public class BoardGames extends Game{
     private String Material;
